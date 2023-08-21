668ea768a8852df966dc95f0ce1786634325bfb5
Simon Hellmann <Simon.Hellmann@outlook.com>
2023-08-21 22:22:50 +0200

    Created Wix Installer.

--- HagisYDNSUpdater/Properties/AssemblyInfo.cs
+++ HagisYDNSUpdater/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HagisYDNSUpdater")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
